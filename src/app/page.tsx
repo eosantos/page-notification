5a32eb814147200c430fa55c2a05d619b14567e0
Eduardo Oliveira <eos.eduardooliveira@gmail.com>
2023-05-21 00:31:04 -0300

    Install fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
+
 // import { BellIcon, UserIcon } from '@heroicons/react/outline';
 
 export default function Home() {
-  return <h1>Oi</h1>
+  return <h1 className="font-sans text-4xl font-bold">TESTE</h1>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,6 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: 'var(--font-roboto)',
-        alt: 'var(--font-bai-jamjuree)',
       },
 
       colors: {
