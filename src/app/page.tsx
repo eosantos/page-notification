32a36ea83f80022029c05115bbefb27a4ea6ca45
Eduardo Oliveira <eos.eduardooliveira@gmail.com>
2023-05-21 02:02:31 -0300

    created component header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
+import { Header } from '@/components/Header'
 import React from 'react'
 
 // import { BellIcon, UserIcon } from '@heroicons/react/outline';
 
 export default function Home() {
   return (
-    <h1 className="bg-violet-700 font-sans text-4xl font-bold opacity-90">
-      TESTE
-    </h1>
+    <div>
+      <Header />
+    </div>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,31 @@
+import { Menu, Bell } from 'lucide-react'
+import Image from 'next/image'
+import logoProz from '../assets/logo-proz.svg'
+import avatarTheacher from '../assets/avatar-teacher.svg'
+
+export function Header() {
+  return (
+    <header className="flex items-center justify-between bg-violet-700 px-4 py-4">
+      <div className="flex items-center">
+        <div className="ml-8 mr-4">
+          <Menu className="w-18 text-white " />
+        </div>
+        <div className="mr-4">
+          <Image src={logoProz} alt="Proz" />
+        </div>
+      </div>
+      <div className="mr-16 flex items-center">
+        <button className="mr-4 p-2 text-gray-600 hover:text-gray-900 focus:text-gray-900 focus:outline-none">
+          <Bell className="h-6 w-6 rounded-full bg-white fill-violet-700 p-1" />
+        </button>
+        <div className="flex items-center">
+          <Image src={avatarTheacher} alt="Proz" />
+          <span className=" ml-4 font-medium text-white">
+            <h1 className="font-light">João da Silva</h1>
+            <span>Professor</span>
+          </span>
+        </div>
+      </div>
+    </header>
+  )
+}
