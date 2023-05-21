36a992bb80dfdff42fc2507104a4c58d14105d57
Eduardo Oliveira <eos.eduardooliveira@gmail.com>
2023-05-21 00:57:59 -0300

    added colors tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,9 @@ import React from 'react'
 // import { BellIcon, UserIcon } from '@heroicons/react/outline';
 
 export default function Home() {
-  return <h1 className="font-sans text-4xl font-bold">TESTE</h1>
+  return (
+    <h1 className="bg-violet-700 font-sans text-4xl font-bold opacity-90">
+      TESTE
+    </h1>
+  )
 }
